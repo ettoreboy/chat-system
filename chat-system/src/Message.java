ce8165179b01da1bc5ccf65ca7256140f58b62f5
PlatinumJesus <cipettaro@gmail.com>
2014-04-06 15:54:02 +0200

    Message class commit

---
+++ chat-system/src/Message.java
@@ -0,0 +1,72 @@
+import java.net.InetAddress;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Message {
+private String text;
+private String user;
+private Timestamp time;
+private InetAddress sourceIp;
+private int sourcePort;
+
+public Message(String text, String user, InetAddress sourceIp,
+		int sourcePort) {
+	super();
+	this.text = text;
+	this.user = user;
+	SimpleDateFormat f = new SimpleDateFormat("MM/dd/yyy HH:ss");
+	Date d = new Date();
+	f.format(d);
+	this.time = new Timestamp(d.getTime());
+	this.sourceIp = sourceIp;
+	this.sourcePort = sourcePort;
+}
+
+public String getText() {
+	return text;
+}
+
+public void setText(String text) {
+	this.text = text;
+}
+
+public String getUser() {
+	return user;
+}
+
+public void setUser(String user) {
+	this.user = user;
+}
+
+public Date getTime() {
+	return time;
+}
+
+public InetAddress getSourceIp() {
+	return sourceIp;
+}
+
+public void setSourceIp(InetAddress sourceIp) {
+	this.sourceIp = sourceIp;
+}
+
+public int getSourcePort() {
+	return sourcePort;
+}
+
+public void setSourcePort(int sourcePort) {
+	this.sourcePort = sourcePort;
+}
+
+@Override
+public String toString() {
+	return "User["+user+"]Message["+text+"]Datetime["+time+"]SourceIp["
+			+sourceIp+"]SourcePort["+sourcePort+"]";
+}
+
+
+
+	
+}
